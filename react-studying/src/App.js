bb7c0d46582524698f5dbdd386418934fcd664c1
이종상 <helius96@naver.com>
2022-06-26 23:06:06 +0900

    things

--- react-studying/src/App.js
+++ react-studying/src/App.js
@@ -1,10 +1,49 @@
-import Button from "./Button";
+import {useState, useEffect} from "react";
+import styles from "./App.module.css";
+
 
 function App() {
+
+  const [counter, setValue] = useState(0);
+
+  const [keyword, setKeyword] = useState("");
+
+  const onClick = () => setValue((prev) => prev + 1);
+
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+
+  const iRunOnlyOnce = () => {
+     console.log("i run only once!");
+  };
+
+  useEffect(iRunOnlyOnce, []);
+
+  useEffect(() => {
+    if(keyword !== "" && keyword.length >= 5){
+      console.log("SEARCH FOR ", keyword);
+    }
+  }, [keyword]);
+
+  useEffect(() => {
+      console.log("I run when counter active");
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("I run when counter active");
+  }, [counter, keyword]);
+
+
   return (
     <div>
-      <h1>Welcome back to REACT!!!!!</h1>
-      <Button text ={"Continue"}/>
+      <input
+        value = {keyword}
+        onChange = {onChange} 
+        type = "text" 
+        placeholder = "Search here..." />
+      <h1 className = {styles.title}>{counter}</h1>
+      <button onClick = {onClick}>Click me!</button>
     </div>
   );
 }
