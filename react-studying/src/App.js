d1bc4b5c3e534ba93ccb6815b2ceff2949ac5e5b
이종상 <helius96@naver.com>
2022-06-29 22:00:16 +0900

    Published

--- react-studying/src/App.js
+++ react-studying/src/App.js
@@ -1,6 +1,22 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Home from "./routes/Home";
+import Detail from "./routes/Detail"
+
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path = {`${process.env.PUBLIC_URL}/`} element = {<Home />}/>
+        <Route path = {`${process.env.PUBLIC_URL}/movie/:id`} element = {<Detail />}/>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- react-studying/src/routes/Home.js
+++ react-studying/src/routes/Home.js
@@ -20,13 +20,13 @@ function Home(){
       getMovies();
     }, []);
   
-    console.log(movies);
   
     return (  
     <div>
       {loading ? <h1>Loading...</h1> : <div>{movies.map((movie) => 
         <Movie
-          key = {movie.id} 
+          key = {movie.id}
+          id = {movie.id} 
           coverImg ={movie.medium_cover_image} 
           title = {movie.title} 
           summary = {movie.summary} 
